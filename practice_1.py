34de898c4b4b63866200be01878b4e7d41c9af58
YongSeon Park <a14758@naver.com>
2022-04-24 01:05:46 +0900

    practice1_python

---
+++ practice_1.py
@@ -0,0 +1,12 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('2*2', 2*2)
+print('4/2', 4/2)
+
+import math
+print('math.sqrt(4)', math.sqrt(4)) # sqrt = sqaure root 제곱근
+print('math.pow(4,2))', math.pow(4,2)) # pow = 제곱
