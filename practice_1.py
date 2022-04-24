a2d9fccb1dbcd59f3cd33dadb178fb93406fada3
YongSeon Park <a14758@naver.com>
2022-04-24 22:31:32 +0900

    practice1-1_python

--- practice_1.py
+++ practice_1.py
@@ -10,3 +10,6 @@ print('4/2', 4/2)
 import math
 print('math.sqrt(4)', math.sqrt(4)) # sqrt = sqaure root 제곱근
 print('math.pow(4,2))', math.pow(4,2)) # pow = 제곱
+
+import random
+print('random.random()', random.random()) # random() => "함수"
